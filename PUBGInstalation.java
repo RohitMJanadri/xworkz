64e0cca890b7c33263a472409ace316d8a8bddf4
Rohit <90209896+RohitMJanadri@users.noreply.github.com>
2021-09-07 22:14:59 +0530

    Update PUBGInstalation.java

--- PUBGInstalation.java
+++ PUBGInstalation.java
@@ -2,7 +2,7 @@ class PUBGInstalation{
 	public static void main(String[] args){
 		byte RAM=8;
 		if(RAM>=6){
-			System.out.println("we can install");
+			System.out.println("you can install");
 }
 
 }

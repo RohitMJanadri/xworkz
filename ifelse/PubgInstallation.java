a6bb7fffa1b1d2b4cbf48382b504762f0ee44697
Rohit <90209896+RohitMJanadri@users.noreply.github.com>
2021-09-13 12:10:39 +0530

    Add files via upload

---
+++ ifelse/PubgInstallation.java
@@ -0,0 +1,18 @@
+package com.xworkz.controlStatements;
+
+public class PubgInstallation {
+
+	public static void main(String[] args) {
+		byte ram=5;
+		if(ram>=6) {
+			System.out.println("installed succesfully");
+		}
+		else
+		{
+			System.out.println("installed unsuccesfully");
+		}
+			
+
+	}
+
+}

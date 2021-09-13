ad5a4ef761389730dcfee8ed374f79de9b400a9c
Rohit <90209896+RohitMJanadri@users.noreply.github.com>
2021-09-13 12:37:50 +0530

    Add files via upload
    Output : N.R. Narayan Murthy

---
+++ ifelse/CompanyNandF.java
@@ -0,0 +1,19 @@
+package com.xworkz.controlStatements;
+
+public class CompanyNandF {
+
+	public static void main(String[] args) {
+		String CompanyName="Infosys";
+		if(CompanyName=="TCS") {
+			System.out.println("J.R.D Tata");
+		} else if(CompanyName=="Infosys") {
+			System.out.println("N.R. Narayan Murthy");	
+		} else if(CompanyName=="Apple") {
+			System.out.println("Steve Jobs");
+		} else if(CompanyName=="Google") {
+			System.out.println("Larry Page");
+		} else if(CompanyName=="Microsoft") {
+			System.out.println("Bill Gates");
+		}
+	}
+}

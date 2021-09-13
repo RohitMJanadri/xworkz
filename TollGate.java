f1e2372baf482845fc604bf16f534c1432fc8d02
Rohit <90209896+RohitMJanadri@users.noreply.github.com>
2021-09-13 12:07:37 +0530

    Add files via upload

---
+++ TollGate.java
@@ -0,0 +1,19 @@
+package com.xworkz.controlStatements;
+
+public class TollGate {
+
+	public static void main(String[] args) {
+		String registration="jm";
+		if(registration=="KA") {
+			System.out.println("Registration valid");
+		}
+		else
+		{
+			System.out.println("Registration  not valid");
+		}
+			
+
+
+	}
+
+}

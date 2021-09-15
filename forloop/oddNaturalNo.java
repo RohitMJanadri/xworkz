d9c6d61c77167c6cae48f81ec871582e2a2bf61e
Rohit <90209896+RohitMJanadri@users.noreply.github.com>
2021-09-15 22:46:11 +0530

    Add files via upload

---
+++ forloop/oddNaturalNo.java
@@ -0,0 +1,14 @@
+package com.xworkz.controlStatements;
+
+public class oddNaturalNo {
+
+	public static void main(String[] args) {
+		for(int i=0;i<=40;i++)
+		{
+			if(i%2!=0)
+				System.out.println(i+" ");
+		}
+
+	}
+
+}

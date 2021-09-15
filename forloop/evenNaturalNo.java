fefd14f9145d2fc14eabb04be18170b7a7adb95a
Rohit <90209896+RohitMJanadri@users.noreply.github.com>
2021-09-15 22:42:45 +0530

    Add files via upload

---
+++ forloop/evenNaturalNo.java
@@ -0,0 +1,14 @@
+package com.xworkz.controlStatements;
+
+public class evenNaturalNo {
+
+	public static void main(String[] args) {
+		for(int i=0;i<=30;i++)
+		{
+			if(i%2==0)
+				System.out.println(i+" ");
+		}
+
+	}
+
+}

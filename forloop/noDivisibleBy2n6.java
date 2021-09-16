8e8c42ffc960692c04fb16fa775024c7105c7ea9
Rohit <90209896+RohitMJanadri@users.noreply.github.com>
2021-09-16 08:34:44 +0530

    Add files via upload

---
+++ forloop/noDivisibleBy2n6.java
@@ -0,0 +1,17 @@
+package com.xworkz.controlStatements;
+
+public class noDivisibleBy2n6 {
+
+	public static void main(String[] args) {
+		for(int i=0;i<=100;i++)
+		{
+			if(i%2==0 && i%6==0)
+				System.out.print(i+" ");
+			
+		}
+		
+		
+
+	}
+
+}
